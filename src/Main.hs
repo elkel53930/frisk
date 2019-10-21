c7b48b291ad25770a74421701a15b3a5d6fda9c3
Kazuki Iida <elkel53930@gmail.com>
2019-10-22 01:17:04 +0900

    コード整理

--- src/Main.hs
+++ src/Main.hs
@@ -49,30 +49,33 @@ proc =
 initState = State [P0,Q0] 0 $ SharedVars Unlocked Unlocked
 
 count :: (a->Bool) -> [a] -> Int
-count f xs = (+) 1 . length $ takeWhile (not . f) xs
+count f = (+1) . length . takeWhile (not . f)
 
 index :: Location -> Int
 index name = count (\(l, _) -> l == name) proc
 
 dotProcess :: String
-dotProcess = [i|digraph {\n#{dotLocations proc}#{dotTrans proc}}|] where
-    dotLocations = concatMap f
-        where f p = [i| \t#{index loc}[label="#{show loc}"];\n|] where loc = fst p
-    dotTrans ps = concatMap f ps
-        where f (from,ts) = concatMap g ts where g (Trans label to _ _) = [i|\t#{index from} -> #{index to}[label="#{label}"];\n|]
+dotProcess = [i|digraph {\n#{concatMap f proc}#{concatMap g proc}}|] where
+    f p = [i| \t#{index loc}[label="#{loc}"];\n|] where loc = fst p
+    g (from,ts) = concatMap h ts where h (Trans label to _ _) = [i|\t#{index from} -> #{index to}[label="#{label}"];\n|]
 
 getTrans :: Location -> [Trans]
 getTrans loc = ts where (_,ts) = head $ dropWhile (\(l,_) -> l /= loc) proc
 
 transition :: State -> (Location, Trans) -> (State,Label)
-transition state (from, t) = (State {locations = update (locations state) from (location t),step = step state + 1, sharedVars = action t $ sharedVars state }, label t)
+transition state (from, t) = 
+    ( State 
+        { locations = update (locations state) from (location t)
+        , step = step state + 1
+        , sharedVars = action t $ sharedVars state }
+    , label t)
     where
         (a,b) = break (==from) $ locations state
         update ls from to = a ++ to : tail b
 
 getTransitionables :: State -> [(Location, Trans)]
 getTransitionables state = concatMap (getAvailableTrans $ sharedVars state) $ locations state
-    where getAvailableTrans var loc = zipWith (,) (repeat loc) $ filter ((flip $ guard) var) $ getTrans loc -- SharedVars -> Location -> [(Location, Trans)]
+    where getAvailableTrans var loc = zipWith (,) (repeat loc) $ filter (flip guard $ var) $ getTrans loc -- SharedVars -> Location -> [(Location, Trans)]
 
 transitionAll :: State -> [(State,Label)]
 transitionAll s = map (transition s) $ getTransitionables s
@@ -89,7 +92,7 @@ search ((state:que), hash, logs) = search
         news = filter (\x -> not $ Set.member x hash) nexts
 
 isDeadlock :: State -> Bool
-isDeadlock s = null $ getTransitionables s
+isDeadlock = null . getTransitionables
 
 decoration :: State -> String
 decoration state = 
@@ -100,11 +103,9 @@ decoration state =
     else ""
 
 dotResult :: [State] -> Logs -> String
-dotResult states logs = [i| digraph {\n#{dotStates states}#{dotLogs states logs}}|] where
-    dotStates states = concatMap f states
-        where f s = [i| \t#{count (==s) states}[label="#{s}"#{decoration s}];\n|]
-    dotLogs states logs = concatMap f logs
-        where f (lbl,from,to) = [i|\t#{count (==from) states} -> #{count (==to) states}[label="#{lbl}"];\n|]
+dotResult states logs = [i| digraph {\n#{concatMap f states}#{concatMap g logs}}|]
+    where f s = [i| \t#{count (==s) states}[label="#{s}"#{decoration s}];\n|]
+          g (lbl,from,to) = [i|\t#{count (==from) states} -> #{count (==to) states}[label="#{lbl}"];\n|]
 
 main = do
     writeFile "output/process.dot" dotProcess
@@ -115,5 +116,5 @@ main = do
     SP.createProcess (SP.proc "dot" ["-Tpng", "output/transition.dot", "-o", "output/transition.png"])
     SP.createProcess (SP.proc "evince" ["output/process.pdf"])
     SP.createProcess (SP.proc "evince" ["output/transition.pdf"])
-    putStrLn $ (show $ Set.size hash) ++ " states."
-    putStrLn $ (show $ length logs) ++ " transitions."
+    putStrLn [i|#{Set.size hash} states.|]
+    putStrLn [i|#{length logs} transitions.|]
