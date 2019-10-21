b805ef036280d64220b84d0b9cec4c990560d1bd
Kazuki Iida <k-iida@smartrobotics.jp>
2019-10-21 15:36:12 +0900

    結果を出力

--- src/Main.hs
+++ src/Main.hs
@@ -15,14 +15,16 @@ type Guard = SharedVars -> Bool
 type Action = SharedVars -> SharedVars
 data Trans = Trans {label :: Label, location :: Location, guard :: Guard, action :: Action} 
 type Process = [(Location, [Trans])]
-data State = State {locations :: [Location], sharedVars :: SharedVars} deriving (Show,Ord,Eq)
+data State = State {locations :: [Location], sharedVars :: SharedVars} deriving (Ord,Eq)
 type Queue = [State]
 type Hash = Set.Set State
-type Logs = [(State,State)]
+type Logs = [(Label,State,State)]
 data Location = P0 | P1 | P2 | P3 | Q0 | Q1 | Q2 | Q3 deriving (Show,Eq,Ord)
 
 instance Show Trans where
     show t = "(Transition " ++ label t ++ " : to " ++ (show $ location t) ++ ")"
+instance Show State where
+    show s = (show $ locations s) ++ "\\n" ++ (dropWhile (/='{') . show $ sharedVars s)
 
 proc :: Process
 proc = 
@@ -38,32 +40,31 @@ proc =
 
 initState = State [P0,Q0] $ SharedVars 0 0 0
 
+count :: (a->Bool) -> [a] -> String
+count f xs = show $ (+) 1 (length $ takeWhile (not . f) xs)
+
 index :: Location -> String
-index name = show . (+) 1 $ length $ takeWhile (\(l, _) -> l /= name) proc
+index name = count (\(l, _) -> l == name) proc
 
-dot :: String
-dot = "digraph {\n" ++ dotLocations proc ++ dotTrans proc ++ "}"
-
-dotLocations :: Process -> String
-dotLocations = concatMap f
-    where f p = "\t" ++  index loc ++ "[label=\"" ++ show loc ++ "\"];\n" where loc = fst p
-
-dotTrans :: Process -> String
-dotTrans ps = concatMap f ps
-    where f (from,ts) = concatMap g ts where g (Trans label to _ _) = "\t" ++  index from ++ " -> " ++ index to ++ "[label=\"" ++ label ++ "\"];\n"
+dotProcess :: String
+dotProcess = "digraph {\n" ++ dotLocations proc ++ dotTrans proc ++ "}" where
+    dotLocations = concatMap f
+        where f p = "\t" ++  index loc ++ "[label=\"" ++ show loc ++ "\"];\n" where loc = fst p
+    dotTrans ps = concatMap f ps
+        where f (from,ts) = concatMap g ts where g (Trans label to _ _) = "\t" ++  index from ++ " -> " ++ index to ++ "[label=\"" ++ label ++ "\"];\n"
 
 getTrans :: Location -> [Trans]
 getTrans loc = ts where (_,ts) = head $ dropWhile (\(l,_) -> l /= loc) proc
 
-transition :: State -> (Location, Trans) -> State
-transition state (from, t) = State {locations = update (locations state) from (location t), sharedVars = action t $ sharedVars state }
+transition :: State -> (Location, Trans) -> (State,Label)
+transition state (from, t) = (State {locations = update (locations state) from (location t), sharedVars = action t $ sharedVars state }, label t)
     where update ls from to = a ++ to : tail b where (a,b) = break (==from) $ locations state
 
 getTransitionables :: State -> [(Location, Trans)]
 getTransitionables state = concatMap (getAvailableTrans $ sharedVars state) $ locations state
     where getAvailableTrans var loc = zipWith (,) (repeat loc) $ filter ((flip $ guard) var) $ getTrans loc -- SharedVars -> Location -> [(Location, Trans)]
 
-transitionAll :: State -> [State]
+transitionAll :: State -> [(State,Label)]
 transitionAll s = map (transition s) $ getTransitionables s
 
 search :: (Queue, Hash, Logs) -> (Queue, Hash, Logs)
@@ -71,19 +72,24 @@ search ([],hash,logs) = ([],hash,logs)
 search ((state:que), hash, logs) = search
     ( que ++ news
     , Set.union hash $ Set.fromList news
-    , logs ++ zipWith (,) (repeat state) nexts
+    , logs ++ zipWith3 (,,) lbls (repeat state) nexts
     )
     where
-        nexts = transitionAll state
+        (nexts,lbls) = unzip $ transitionAll state
         news = filter (\x -> not $ Set.member x hash) nexts
 
-display :: (Queue, Hash, Logs) -> String
-display (q,h,l) = "Queue = [\n" ++ dispS q ++ "]\n\nHash = [\n" ++ dispS (Set.toList h) ++ "]\n\nLogs = [\n" ++ dispL
-    where
-        dispS = concatMap (\state -> "\t" ++ show state ++ "\n")
-        dispL = concatMap (\(from,to) -> "\t" ++ (show $ locations from) ++ " -> " ++ (show $ locations to) ++ " " ++ (show $ sharedVars to) ++  "\n") l
+dotResult :: [State] -> Logs -> String
+dotResult states logs = "digraph {\n" ++ dotStates states ++ dotLogs states logs ++ "}" where
+    dotStates states = concatMap f states
+        where f s = "\t" ++  (count (==s) states) ++ "[label=\"" ++ show s ++ "\"];\n"
+    dotLogs states logs = concatMap f logs
+        where f (lbl,from,to) = "\t" ++  (count (==from) states) ++ " -> " ++ (count (==to) states) ++ "[label=\"" ++ lbl ++ "\"];\n"
 
 main = do
-    writeFile "process.dot" dot
+    writeFile "process.dot" dotProcess
     SP.createProcess (SP.proc "dot" ["-Tpdf", "process.dot", "-o", "process.pdf"])
-    SP.createProcess (SP.proc "evince" ["process.pdf"])    
+    let (_,hash,logs) = search ([initState],Set.singleton initState,[])
+    writeFile "transition.dot" $ dotResult (Set.toList hash) logs
+    SP.createProcess (SP.proc "dot" ["-Tpdf", "transition.dot", "-o", "transition.pdf"])
+
+    SP.createProcess (SP.proc "evince" ["transition.pdf"])
